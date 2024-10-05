3c8f414114bd60066d9e6e4738d03f4fb7c871e5
williamsmata <williams.rm99@gmail.com>
2024-10-05 18:20:25 -0400

    Improve button

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -18,8 +18,11 @@ import { sendMessageSchema } from "@/schema/send-message.schema";
 import { sendMessage } from "@/actions/sendMessage";
 import { motion } from "framer-motion";
 import { Toaster } from "@/adapter/sonner.adapter";
+import { useState } from "react";
 
 export default function ContactForm() {
+  const [disableByLimit, setDisableByLimit] = useState(false);
+
   const form = useForm<z.infer<typeof sendMessageSchema>>({
     resolver: zodResolver(sendMessageSchema),
     defaultValues: {
@@ -28,6 +31,14 @@ export default function ContactForm() {
       message: "",
     },
   });
+
+  const buttonMessage = form.formState.isSubmitting
+    ? "Sending..."
+    : disableByLimit
+      ? "You have reached the limit"
+      : form.formState.isSubmitSuccessful
+        ? "Message sent successfully"
+        : "Send Message";
 
   async function onSubmit(values: z.infer<typeof sendMessageSchema>) {
     const { errors } = await sendMessage(values);
@@ -41,6 +52,17 @@ export default function ContactForm() {
               .join(", ");
 
       Toaster.error(error, { position: "bottom-left" });
+
+      if (error.includes("reached the limit")) {
+        setDisableByLimit(true);
+        setTimeout(
+          () => {
+            setDisableByLimit(false);
+          },
+          1000 * 60 * 5,
+        );
+      }
+
       return;
     }
 
@@ -115,16 +137,26 @@ export default function ContactForm() {
           )}
         />
 
-        <Button
-          disabled={form.formState.isSubmitting}
-          className="w-full"
-          type="submit"
-          onClick={() => {
-            form.clearErrors();
-          }}
+        <motion.div
+          initial={{ scale: 1 }}
+          animate={{ scale: form.formState.isSubmitting ? 0.95 : 1 }}
+          transition={{ type: "spring", stiffness: 300, damping: 20 }}
         >
-          {form.formState.isSubmitting ? "Sending..." : "Send Message"}
-        </Button>
+          <Button
+            disabled={
+              form.formState.isSubmitting ||
+              form.formState.isSubmitSuccessful ||
+              disableByLimit
+            }
+            className="w-full"
+            type="submit"
+            onClick={() => {
+              form.clearErrors();
+            }}
+          >
+            {buttonMessage}
+          </Button>
+        </motion.div>
       </form>
     </Form>
   );
