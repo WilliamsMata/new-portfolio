50464c38c86e7e618ca863dec81321726d902872
williamsmata <williams.rm99@gmail.com>
2024-10-05 21:54:32 -0400

    Increase margin for header in Contact component for improved visual spacing and layout consistency

--- src/components/views/Contact.tsx
+++ src/components/views/Contact.tsx
@@ -6,7 +6,7 @@ export const Contact = () => {
   return (
     <section className="flex flex-col items-center">
       <BackgroundBeamsWithCollision>
-        <h2 className="mb-4 bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-4xl font-semibold text-transparent">
+        <h2 className="mb-12 bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-4xl font-semibold text-transparent">
           Contact
         </h2>
 
