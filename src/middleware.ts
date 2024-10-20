4ef2fc6195702ce4f8f655113fb8b66327c19e40
williamsmata <williams.rm99@gmail.com>
2024-10-20 10:02:14 -0400

    Refactor locale handling in middleware for better compatibility with i18n by spreading locales into a new array

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,8 +11,7 @@ function getLocale(request: NextRequest): string | undefined {
   const negotiatorHeaders: Record<string, string> = {};
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
-  // @ts-expect-error locales are readonly
-  const locales: string[] = i18n.locales;
+  const locales = [...i18n.locales];
 
   // Use negotiator and intl-localematcher to get best locale
   const languages = new Negotiator({ headers: negotiatorHeaders }).languages(
