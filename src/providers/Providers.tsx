eb7e5d5e6035caabdce3ff3bdfa4ef986fbd43a4
williamsmata <williams.rm99@gmail.com>
2024-11-04 11:12:30 -0400

    Fix types next theme provider

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -4,14 +4,5 @@ import * as React from "react";
 import { ThemeProvider } from "./ThemeProvider";
 
 export function Providers({ children }: React.PropsWithChildren) {
-  return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
-  );
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
